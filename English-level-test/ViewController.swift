93e0e9578daa8a096ceda4330794eec87e35d38a
MaciejDubowik <maciejxpoland@gmail.com>
2023-02-24 01:23:57 +0100

    storyboard question view

--- English-level-test/ViewController.swift
+++ English-level-test/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        button1.layer.cornerRadius = 10
+        button1.layer.borderColor = UIColor.systemBlue.cgColor
+        button1.layer.borderWidth = 2
     }
-
+    
 
 }
 
